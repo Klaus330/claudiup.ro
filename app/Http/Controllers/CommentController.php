6d568ccb0b402aa64cef4a0cb1c049281a18b12c
Claudiu Popa <claudiup340@gmail.com>
2018-10-30 19:00:35 +0200

    An unauthenticated user can leave a comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -9,7 +9,7 @@ class CommentController extends Controller
 {
     public function __construct()
     {
-        $this->middleware("auth");
+        $this->middleware("auth")->except('store');
     }
 
     /**
