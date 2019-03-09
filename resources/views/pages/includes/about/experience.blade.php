22d3f597f713f4f8fa873c8e28a1eea038f32e4e
Klaus33 <claudiup340@gmail.com>
2019-03-09 20:31:05 +0200

    Move the mobility certificates to experience section

--- resources/views/pages/includes/about/experience.blade.php
+++ resources/views/pages/includes/about/experience.blade.php
@@ -16,6 +16,28 @@
             </div>
 			<!-- Single Experience Ends -->
             <span class="separator"></span>
+            <!-- Single Education Starts -->
+            <div class="resume-content">
+                <h6 class="uppercase"><span>Europass mobility certificate </span> </h6>
+                <span class="date"><i class="fa fa-calendar-o"></i> January 2015 - February 2015</span>
+                <p>Europass mobility certificate for participation in Erasmus+ project "English Through Entrepreneurship" in Belgium</p>
+            </div>
+            <!-- Single Education Ends -->
+            <span class="separator"></span>
+            <!-- Single Education Starts -->
+            <div class="resume-content">
+                <h6 class="uppercase"><span>Europass mobility certificate  </span> </h6>
+                <span class="date"><i class="fa fa-calendar-o"></i> 16 February 2017 - 23 February 2017</span>
+                <p>Europass mobility certificate for participation in Erasmus+ project "English Through Entrepreneurship" in Norway</p>
+            </div>
+            <span class="separator"></span>
+            <!-- Single Education Starts -->
+            <div class="resume-content">
+                <h6 class="uppercase"><span>Europass mobility certificate</span> </h6>
+                <span class="date"><i class="fa fa-calendar-o"></i> 11 March 2018 - 18 March 2018</span>
+                <p>Europass mobility certificate for participation in Erasmus+ project "English Through Entrepreneurship" in France</p>
+            </div>
+            <!-- Single Education Ends -->
         </div>
     </div>
 	<!-- Experience Content Starts -->
