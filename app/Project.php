a0ffcbd099dfa7697b6e498dfd7ca0d2dd4dd0f8
Klaus33 <claudiup340@gmail.com>
2019-03-31 19:18:36 +0300

    make youtube links secure

--- app/Project.php
+++ app/Project.php
@@ -86,7 +86,7 @@ class Project extends Model
     {
         $string     = $url;
         $search     = '#(.*?)(?:href="https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch?.*?v=))([\w\-]{10,12}).*#x';
-        $replace    = 'http://www.youtube.com/embed/$2';
+        $replace    = 'https://www.youtube.com/embed/$2';
         $url        = preg_replace($search, $replace, $string);
         return $url;
     }
