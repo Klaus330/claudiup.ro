9655767d6f35454831b5db4e53fc014fa2b48e13
Klaus33 <claudiup340@gmail.com>
2019-09-26 09:18:54 +0300

    Add images for the secret game

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js').extract(['vue'])
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .copy('resources/images', 'public/images');
